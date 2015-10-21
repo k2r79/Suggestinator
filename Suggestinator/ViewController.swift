8ed4a546fa6dcc9aa7aa034064bee03a9d3dd0b5
Vincent Kelleher <k2r79@hotmail.fr>
2015-10-21 10:03:08 +0200

    Initial Commit

---
+++ Suggestinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Suggestinator
+//
+//  Created by Vincent Kelleher on 21/10/2015.
+//  Copyright © 2015 Vincent Kelleher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
